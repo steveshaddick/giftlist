2f39c2f2b04f48fda3da72844ac561778478e55d
Steve Shaddick <steveshaddick@gmail.com>
2022-08-03 09:05:17 -0400

    added email capability

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.gmail.username,
+    password:             Rails.application.credentials.gmail.password,
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5,
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
